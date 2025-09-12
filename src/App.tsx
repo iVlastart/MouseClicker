18f47985b2993115d5b64971a6f250ef5e90f3f6
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-09-12 18:36:57 +0200

    Adding the functionality of saving your mouses

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Game } from "./components/game/game"
 import { Header } from "./components/header"
 
 function App() {
-  const [mouseCount, setMouseCount] = useState<number>(0);
+  const [mouseCount, setMouseCount] = useState<number>(Number(localStorage.getItem('mouseCount'))?? 0);
+
+  useEffect(()=>{
+    const beforeUnload = (e: BeforeUnloadEvent)=>{
+      e.preventDefault();
+    };
+
+    window.addEventListener('beforeunload', beforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnload);
+    };
+  },[])
   return (
     <>
       <Header/>
