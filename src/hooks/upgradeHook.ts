39b7dda65f0cabe31226e5ed3b05b5db931ce44e
Vlasta Vašut <vasutvlastimil@gmail.com>
2025-09-13 21:57:43 +0200

    Fixed prices when getting them from local storage

--- src/hooks/upgradeHook.ts
+++ src/hooks/upgradeHook.ts
@@ -2,10 +2,16 @@ import { useState } from "react"
 
 export const useUpgradesStates = ()=>{
     const [mouseTrapCount, setMouseTrapCount] = useState(Number(localStorage.getItem('mouseTrapCount'))??0);
-    const [mouseTrapPrice, setMouseTrapPrice] = useState(Number(localStorage.getItem('mouseTrapPrice'))??15);
+    const initialMouseTrapPrice = Number(localStorage.getItem('mouseTrapPrice'));
+    const [mouseTrapPrice, setMouseTrapPrice] = useState(
+        initialMouseTrapPrice > 0 ? initialMouseTrapPrice : 15
+    );
     
     const [cheeseCount, setCheeseCount] = useState(Number(localStorage.getItem('cheeseCount'))??0);
-    const [cheesePrice, setCheesePrice] = useState(Number(localStorage.getItem('cheesePrice'))??100);
+    const initialCheesePrice = Number(localStorage.getItem('cheesePrice'));
+    const [cheesePrice, setCheesePrice] = useState(
+        initialCheesePrice>0?initialCheesePrice:100
+    );
     
 
     return{
